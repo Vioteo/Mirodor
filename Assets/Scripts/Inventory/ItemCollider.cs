c5106200bf68f2ab6670e71e80c666064cc2b7fe
Vioteo <prostosokol@gmail.com>
2019-12-17 01:35:00 +0300

    Added inventory and a lot of menu improvements

---
+++ Assets/Scripts/Inventory/ItemCollider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemCollider : MonoBehaviour
+{
+    public Inventory inventory;
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Debug.Log("colided");
+        Rigidbody body = hit.collider.attachedRigidbody;
+        IInventoryItem item = hit.collider.GetComponent<IInventoryItem>();
+        if (item != null)
+        {
+            inventory.AddItem(item);
+        }
+    }
+}
